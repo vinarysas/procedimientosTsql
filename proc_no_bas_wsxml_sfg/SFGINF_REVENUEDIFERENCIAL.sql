e3b10cccc3a21f61562d1036c59706fd10d26365
jonathanCarvajalG <joan_tom@hotmail.com>
2018-02-06 16:05:05 -0500

    Procedimientos Incompletos
    Se completan los procedimientos y se prueban dando como resultado la ejecución correcta de los mismos

--- proc_no_bas_wsxml_sfg/SFGINF_REVENUEDIFERENCIAL.sql
+++ proc_no_bas_wsxml_sfg/SFGINF_REVENUEDIFERENCIAL.sql
@@ -72,9 +72,57 @@ CREATE     PROCEDURE WSXML_SFG.SFGINF_REVENUEDIFERENCIAL_GetRevenueYTDNoKioskos(
                                    @p_CODLINEADENEGOCIO      NUMERIC(22,0),
                                    @pg_CADENA                NVARCHAR(2000),
                                    @pg_ALIADOESTRATEGICO     NVARCHAR(2000),
-                                  @pg_PRODUCTO              NVARCHAR(2000)
-                                   ) AS
- GO
+                                   @pg_PRODUCTO              NVARCHAR(2000)) AS
+ BEGIN
+    DECLARE @sFECHACCLO DATETIME;
+    DECLARE @sFECHAFRST DATETIME;
+    DECLARE @sFECHALAST DATETIME;
+   
+  SET NOCOUNT ON;
+    SELECT @sFECHACCLO = FECHAEJECUCION FROM WSXML_SFG.CICLOFACTURACIONPDV WHERE ID_CICLOFACTURACIONPDV = @p_CODCICLOFACTURACIONPDV;
+    EXEC WSXML_SFG.SFG_PACKAGE_GetMonthRange @sFECHACCLO, @sFECHAFRST OUT, @sFECHALAST OUT
+      SELECT /*+ push_pred(PRF) */
+             AES.NOMALIADOESTRATEGICO                       AS ALIADO,
+             AGP.NOMAGRUPACIONPRODUCTO                      AS PADRE,
+             PRD.CODIGOGTECHPRODUCTO                        AS CODIGOPRODUCTO,
+             PRD.NOMPRODUCTO                                AS SUBPRODUCTO,
+             SUM(NumIngresos - NumAnulaciones)              AS CANTIDADINGRESOS,
+             SUM(IngresosBrutosNoRedondeo)                  AS INGRESOS,
+             SUM(RevenueBase)                               AS REVENUBASE,
+             SUM(RevenueTransaccion)                        AS RANGOS,
+             SUM(RevenueFijo)                               AS FIJO,
+             SUM(RevenueTotal)                              AS REVENUETOTAL,
+             SUM(IngresoCorporativo)                        AS INGCORPORATIVO,
+             SUM(IngresoLocal)                              AS INGLOCAL,
+             SUM(CASE WHEN PRF.CODTIPOCONTRATOPDV IN (1, 2)
+                      THEN Comision ELSE 0 END)             AS INGRESOPDVADMIN,
+             SUM(CASE WHEN PRF.CODTIPOCONTRATOPDV = 3
+                      THEN Comision ELSE 0 END)             AS INGRESOPDVCOLAB,
+             SUM(EgresoLocal)                               AS COSTOVENTA,
+             SUM(CostoICA)                                  AS ICA,
+             SUM(CostoEtesa)                                AS COMISIONETESA,
+             SUM(CostoIC)                                   AS IC,
+             SUM(CostoICAIC)                                AS ICAIC,
+             SUM(CostoBadDebt)                              AS BADDEBT,
+             SUM(CostoMercadeoVenta + CostoMercadeoRevenue) AS MERCADEO,
+             SUM(CostoIvaNoDescontable)                     AS IVANODESC,
+             SUM(CostoIC108)                                AS IC108,
+             SUM(CostoICAIC108)                             AS ICAIC108,
+             SUM(UtilidadParcial)                           AS UTILIDADPARCIAL
+      FROM WSXML_SFG.VW_REVENUE_DIARIO PRF
+      INNER JOIN WSXML_SFG.PRODUCTO               PRD ON (PRD.ID_PRODUCTO           = PRF.CODPRODUCTO)
+      INNER JOIN WSXML_SFG.ALIADOESTRATEGICO      AES ON (AES.ID_ALIADOESTRATEGICO  = PRD.CODALIADOESTRATEGICO)
+      INNER JOIN WSXML_SFG.AGRUPACIONPRODUCTO     AGP ON (AGP.ID_AGRUPACIONPRODUCTO = PRD.CODAGRUPACIONPRODUCTO)
+      INNER JOIN WSXML_SFG.PUNTODEVENTA           PDV ON (PDV.ID_PUNTODEVENTA       = PRF.CODPUNTODEVENTA)
+      WHERE PRF.FECHAARCHIVO BETWEEN CONVERT(VARCHAR,FORMAT(@sFECHAFRST, 'yyyy')+'-01-01',103) AND @sFECHALAST
+        AND PRF.CODLINEADENEGOCIO = @p_CODLINEADENEGOCIO
+        AND PDV.CODTIPONEGOCIO    <> 7
+      GROUP BY AES.NOMALIADOESTRATEGICO, AGP.NOMAGRUPACIONPRODUCTO, PRD.CODIGOGTECHPRODUCTO, PRD.NOMPRODUCTO
+      ORDER BY AGP.NOMAGRUPACIONPRODUCTO, PRD.CODIGOGTECHPRODUCTO;
+  END;
+
+  GO
+ 
   IF OBJECT_ID('WSXML_SFG.SFGINF_REVENUEDIFERENCIAL_GetRevenueRedAEL', 'P') IS NOT NULL
   DROP PROCEDURE WSXML_SFG.SFGINF_REVENUEDIFERENCIAL_GetRevenueRedAEL;
 GO
@@ -83,9 +131,54 @@ CREATE     PROCEDURE WSXML_SFG.SFGINF_REVENUEDIFERENCIAL_GetRevenueRedAEL(@p_COD
                              @p_CODLINEADENEGOCIO      NUMERIC(22,0),
                              @pg_CADENA                NVARCHAR(2000),
                              @pg_ALIADOESTRATEGICO     NVARCHAR(2000),
-                            @pg_PRODUCTO              NVARCHAR(2000)
-                             ) AS
- GO
+                             @pg_PRODUCTO              NVARCHAR(2000)) AS
+ BEGIN
+    DECLARE @sFECHACCLO DATETIME;
+    DECLARE @sFECHAFRST DATETIME;
+    DECLARE @sFECHALAST DATETIME;
+   
+  SET NOCOUNT ON;
+    SELECT @sFECHACCLO = FECHAEJECUCION FROM WSXML_SFG.CICLOFACTURACIONPDV WHERE ID_CICLOFACTURACIONPDV = @p_CODCICLOFACTURACIONPDV;
+    EXEC WSXML_SFG.SFG_PACKAGE_GetMonthRange @sFECHACCLO, @sFECHAFRST OUT, @sFECHALAST OUT
+      SELECT /*+ push_pred(PRF) */
+             AES.NOMALIADOESTRATEGICO                       AS ALIADO,
+             AGP.NOMAGRUPACIONPRODUCTO                      AS PADRE,
+             PRD.CODIGOGTECHPRODUCTO                        AS CODIGOPRODUCTO,
+             PRD.NOMPRODUCTO                                AS SUBPRODUCTO,
+             SUM(NumIngresos - NumAnulaciones)              AS CANTIDADINGRESOS,
+             SUM(IngresosBrutosNoRedondeo)                  AS INGRESOS,
+             SUM(RevenueBase)                               AS REVENUBASE,
+             SUM(RevenueTransaccion)                        AS RANGOS,
+             SUM(RevenueFijo)                               AS FIJO,
+             SUM(RevenueTotal)                              AS REVENUETOTAL,
+             SUM(IngresoCorporativo)                        AS INGCORPORATIVO,
+             SUM(IngresoLocal)                              AS INGLOCAL,
+             SUM(CASE WHEN PRF.CODTIPOCONTRATOPDV IN (1, 2)
+                      THEN Comision ELSE 0 END)             AS INGRESOPDVADMIN,
+             SUM(CASE WHEN PRF.CODTIPOCONTRATOPDV = 3
+                      THEN Comision ELSE 0 END)             AS INGRESOPDVCOLAB,
+             SUM(EgresoLocal)                               AS COSTOVENTA,
+             SUM(CostoICA)                                  AS ICA,
+             SUM(CostoEtesa)                                AS COMISIONETESA,
+             SUM(CostoIC)                                   AS IC,
+             SUM(CostoICAIC)                                AS ICAIC,
+             SUM(CostoBadDebt)                              AS BADDEBT,
+             SUM(CostoMercadeoVenta + CostoMercadeoRevenue) AS MERCADEO,
+             SUM(CostoIvaNoDescontable)                     AS IVANODESC,
+             SUM(CostoIC108)                                AS IC108,
+             SUM(CostoICAIC108)                             AS ICAIC108,
+             SUM(UtilidadParcial)                           AS UTILIDADPARCIAL
+      FROM WSXML_SFG.VW_REVENUE_DIARIO PRF
+      INNER JOIN WSXML_SFG.PRODUCTO               PRD ON (PRD.ID_PRODUCTO           = PRF.CODPRODUCTO)
+      INNER JOIN WSXML_SFG.ALIADOESTRATEGICO      AES ON (AES.ID_ALIADOESTRATEGICO  = PRD.CODALIADOESTRATEGICO)
+      INNER JOIN WSXML_SFG.AGRUPACIONPRODUCTO     AGP ON (AGP.ID_AGRUPACIONPRODUCTO = PRD.CODAGRUPACIONPRODUCTO)
+      WHERE PRF.FECHAARCHIVO BETWEEN CONVERT(VARCHAR,FORMAT(@sFECHAFRST, 'yyyy')+'-01-01',103) AND @sFECHALAST
+        AND PRF.CODLINEADENEGOCIO = @p_CODLINEADENEGOCIO
+        AND PRF.CODREDPDV         = WSXML_SFG.RED_F('AEL')
+      GROUP BY AES.NOMALIADOESTRATEGICO, AGP.NOMAGRUPACIONPRODUCTO, PRD.CODIGOGTECHPRODUCTO, PRD.NOMPRODUCTO
+      ORDER BY AGP.NOMAGRUPACIONPRODUCTO, PRD.CODIGOGTECHPRODUCTO;
+  END;
+GO
 
   IF OBJECT_ID('WSXML_SFG.SFGINF_REVENUEDIFERENCIAL_GetRevenueRedNoAEL', 'P') IS NOT NULL
   DROP PROCEDURE WSXML_SFG.SFGINF_REVENUEDIFERENCIAL_GetRevenueRedNoAEL;
@@ -95,7 +188,52 @@ CREATE     PROCEDURE WSXML_SFG.SFGINF_REVENUEDIFERENCIAL_GetRevenueRedNoAEL(@p_C
                                @p_CODLINEADENEGOCIO      NUMERIC(22,0),
                                @pg_CADENA                NVARCHAR(2000),
                                @pg_ALIADOESTRATEGICO     NVARCHAR(2000),
-                              @pg_PRODUCTO              NVARCHAR(2000)
-                               ) AS
-GO
+                               @pg_PRODUCTO              NVARCHAR(2000)) AS
+ BEGIN
+    DECLARE @sFECHACCLO DATETIME;
+    DECLARE @sFECHAFRST DATETIME;
+    DECLARE @sFECHALAST DATETIME;
+   
+  SET NOCOUNT ON;
+    SELECT @sFECHACCLO = FECHAEJECUCION FROM WSXML_SFG.CICLOFACTURACIONPDV WHERE ID_CICLOFACTURACIONPDV = @p_CODCICLOFACTURACIONPDV;
+    EXEC WSXML_SFG.SFG_PACKAGE_GetMonthRange @sFECHACCLO, @sFECHAFRST OUT, @sFECHALAST OUT
+      SELECT /*+ push_pred(PRF) */
+             AES.NOMALIADOESTRATEGICO                       AS ALIADO,
+             AGP.NOMAGRUPACIONPRODUCTO                      AS PADRE,
+             PRD.CODIGOGTECHPRODUCTO                        AS CODIGOPRODUCTO,
+             PRD.NOMPRODUCTO                                AS SUBPRODUCTO,
+             SUM(NumIngresos - NumAnulaciones)              AS CANTIDADINGRESOS,
+             SUM(IngresosBrutosNoRedondeo)                  AS INGRESOS,
+             SUM(RevenueBase)                               AS REVENUBASE,
+             SUM(RevenueTransaccion)                        AS RANGOS,
+             SUM(RevenueFijo)                               AS FIJO,
+             SUM(RevenueTotal)                              AS REVENUETOTAL,
+             SUM(IngresoCorporativo)                        AS INGCORPORATIVO,
+             SUM(IngresoLocal)                              AS INGLOCAL,
+             SUM(CASE WHEN PRF.CODTIPOCONTRATOPDV IN (1, 2)
+                      THEN Comision ELSE 0 END)             AS INGRESOPDVADMIN,
+             SUM(CASE WHEN PRF.CODTIPOCONTRATOPDV = 3
+                      THEN Comision ELSE 0 END)             AS INGRESOPDVCOLAB,
+             SUM(EgresoLocal)                               AS COSTOVENTA,
+             SUM(CostoICA)                                  AS ICA,
+             SUM(CostoEtesa)                                AS COMISIONETESA,
+             SUM(CostoIC)                                   AS IC,
+             SUM(CostoICAIC)                                AS ICAIC,
+             SUM(CostoBadDebt)                              AS BADDEBT,
+             SUM(CostoMercadeoVenta + CostoMercadeoRevenue) AS MERCADEO,
+             SUM(CostoIvaNoDescontable)                     AS IVANODESC,
+             SUM(CostoIC108)                                AS IC108,
+             SUM(CostoICAIC108)                             AS ICAIC108,
+             SUM(UtilidadParcial)                           AS UTILIDADPARCIAL
+      FROM WSXML_SFG.VW_REVENUE_DIARIO PRF
+      INNER JOIN WSXML_SFG.PRODUCTO               PRD ON (PRD.ID_PRODUCTO           = PRF.CODPRODUCTO)
+      INNER JOIN WSXML_SFG.ALIADOESTRATEGICO      AES ON (AES.ID_ALIADOESTRATEGICO  = PRD.CODALIADOESTRATEGICO)
+      INNER JOIN WSXML_SFG.AGRUPACIONPRODUCTO     AGP ON (AGP.ID_AGRUPACIONPRODUCTO = PRD.CODAGRUPACIONPRODUCTO)
+      WHERE PRF.FECHAARCHIVO BETWEEN CONVERT(VARCHAR,FORMAT(@sFECHAFRST, 'yyyy'),103) AND @sFECHALAST
+        AND PRF.CODLINEADENEGOCIO = @p_CODLINEADENEGOCIO
+        AND PRF.CODREDPDV         <> WSXML_SFG.RED_F('AEL')
+      GROUP BY AES.NOMALIADOESTRATEGICO, AGP.NOMAGRUPACIONPRODUCTO, PRD.CODIGOGTECHPRODUCTO, PRD.NOMPRODUCTO
+      ORDER BY AGP.NOMAGRUPACIONPRODUCTO, PRD.CODIGOGTECHPRODUCTO;
+  END;
+  GO
 
