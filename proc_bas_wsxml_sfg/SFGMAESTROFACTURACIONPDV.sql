5ca1bdc7e775f3c88a909425eb95fafcd0b8a2eb
jonathanCarvajalG <joan_tom@hotmail.com>
2018-02-08 22:54:57 -0500

    Ajuste procedimientos
    Para finalizar los errores se agrego la instruccion RETURN 0

--- proc_bas_wsxml_sfg/SFGMAESTROFACTURACIONPDV.sql
+++ proc_bas_wsxml_sfg/SFGMAESTROFACTURACIONPDV.sql
@@ -151,6 +151,7 @@ GO
 		BEGIN CATCH		
 			SET @msg = '-20054 Error al crear el registro de facturacion de linea de negocio: ' + isnull(ERROR_MESSAGE ( ), '');
 			RAISERROR(@msg, 16, 1);
+			RETURN 0;
 		END CATCH
     END;
 
@@ -1315,6 +1316,7 @@ GO
 		BEGIN CATCH
 			SET @msg = '-20054 Error al crear el registro de facturacion de linea de negocio: ' + isnull(ERROR_MESSAGE ( ) , '');
 			RAISERROR(@msg, 16, 1);
+			RETURN 0
 		END CATCH
 			
     END;
