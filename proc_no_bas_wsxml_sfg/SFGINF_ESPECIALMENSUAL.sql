882086ed7c5337b94c61955e6558e3fb42120c33
jonathanCarvajalG <joan_tom@hotmail.com>
2018-02-17 11:18:28 -0500

    Procedimiento implementado

--- proc_no_bas_wsxml_sfg/SFGINF_ESPECIALMENSUAL.sql
+++ proc_no_bas_wsxml_sfg/SFGINF_ESPECIALMENSUAL.sql
@@ -320,14 +320,71 @@ CREATE     PROCEDURE WSXML_SFG.SFGINF_ESPECIALMENSUAL_FacturacionProcesa(@ID_DET
 GO
 
   
-   IF OBJECT_ID('WSXML_SFG.SFGINF_ESPECIALMENSUAL_ConsolmensualTxLogueoTerminal', 'P') is not null
+  IF OBJECT_ID('WSXML_SFG.SFGINF_ESPECIALMENSUAL_ConsolmensualTxLogueoTerminal', 'P') is not null
   drop PROCEDURE WSXML_SFG.SFGINF_ESPECIALMENSUAL_ConsolmensualTxLogueoTerminal;
 go
 
 create       PROCEDURE WSXML_SFG.SFGINF_ESPECIALMENSUAL_ConsolmensualTxLogueoTerminal(@ID_DETALLETAREAEJEUTADA NUMERIC(22,0), 
                                       @DIA DATETIME,
-                                     @pCODPRODUCTO NUMERIC(22,0))
+                                      @pCODPRODUCTO NUMERIC(22,0))
   as
+  begin 
+    declare @sFECHAFRST DATETIME;
+    declare @sFECHALAST DATETIME;
+    declare @sWEEKDAY INT;
+   
+  set nocount on;
+   EXEC  WSXML_SFG.SFG_PACKAGE_GetMonthRange @DIA, @sFECHAFRST OUT, @sFECHALAST OUT
+	select fechaarchivo  as "Fecha Archivo", codigogtechpuntodeventa as "Numero PDV",
+        nompuntodeventa as "Nombre PDV", suscriptor as "Numero Cedula", horas as "Horas", minutos as "Minutos", 
+        totalminutos as "Total minutos", sum(numerotransacciones) as "Numero Transacciones", 
+        sum(montoventa) as "Monto Venta" , nomregional as "Regional", ciudad as "Ciudad"
+    from ( 
+            SELECT distinct reg.codpuntodeventa,  convert(varchar, e.fechaarchivo) as fechaarchivo, p.codigogtechpuntodeventa ,
+            p.nompuntodeventa,
+            rfr.suscriptor, 
+            cast(round(WSXML_SFG.datediff('mi',max(rfr.fechahoratransaccion), min(rfr.fechahoratransaccion)) /60 ,0) as integer)*-1 as horas,
+            case when 
+            round((cast(round(WSXML_SFG.datediff('mi',min(rfr.fechahoratransaccion), max(rfr.fechahoratransaccion)) /60 ,3) as float) -
+            cast(round(WSXML_SFG.datediff('mi',min(rfr.fechahoratransaccion), max(rfr.fechahoratransaccion)) /60 ,0) as float)) * 60, 0) < 0 then 
+            round((cast(round(WSXML_SFG.datediff('mi',min(rfr.fechahoratransaccion), max(rfr.fechahoratransaccion)) /60 ,3) as float) -
+            cast(round(WSXML_SFG.datediff('mi',min(rfr.fechahoratransaccion), max(rfr.fechahoratransaccion)) /60 ,0) as float)) * 60, 0)*-1
+            else round((cast(round(WSXML_SFG.datediff('mi',min(rfr.fechahoratransaccion), max(rfr.fechahoratransaccion)) /60 ,3) as float) -
+            cast(round(WSXML_SFG.datediff('mi',min(rfr.fechahoratransaccion), max(rfr.fechahoratransaccion)) /60 ,0) as float)) * 60, 0) end as minutos,
+            cast(round(WSXML_SFG.datediff('mi',max(rfr.fechahoratransaccion), min(rfr.fechahoratransaccion)) ,0) as integer) * -1 as totalminutos,
+            isnull(case when reg.codpuntodeventa = rf.codpuntodeventa --and rfr.suscriptor = reg.suscriptor
+              then  sum(reg.totaltx)  end, 0) as numerotransacciones, 
+             isnull(case when reg.codpuntodeventa = rf.codpuntodeventa  
+                then  isnull(sum(reg.totalventas), 0) end, 0)  as Montoventa,
+            r.nomregional , 
+            (case when min(CONVERT(VARCHAR(8), rfr.fechahoratransaccion,108)) = min(CONVERT(VARCHAR(8), rfr.fechahoratransaccion,108)) 
+            then c.nomciudad end) as ciudad 
+            FROM WSXML_SFG.REGISTROFACTREFERENCIA rfr
+            inner join WSXML_SFG.registrofacturacion rf on rf.id_registrofacturacion = rfr.codregistrofacturacion
+            inner join WSXML_SFG.entradaarchivocontrol e on rf.codentradaarchivocontrol = e.id_entradaarchivocontrol 
+            inner join WSXML_SFG.puntodeventa p on p.id_puntodeventa = rf.codpuntodeventa
+            inner join WSXML_SFG.regional r on r.id_regional = p.codregional 
+            inner join WSXML_SFG.ciudad c on c.id_ciudad = p.codciudad
+            left outer join (select case when codtiporegistro in (1,3) then sum(regf.numtransacciones) else 0 end as totaltx, 
+                 case when codtiporegistro in (1,3) then sum(regf.valortransaccion) else 0 end as totalventas, 
+                 regf.codentradaarchivocontrol, regf.codpuntodeventa, rfr.fechahoratransaccion, rfr.suscriptor
+                 from WSXML_SFG.registrofacturacion regf 
+                 inner join WSXML_SFG.entradaarchivocontrol ent on ent.id_entradaarchivocontrol = regf.codentradaarchivocontrol
+                 inner join WSXML_SFG.registrofactreferencia rfr on rfr.codregistrofacturacion = regf.id_registrofacturacion
+                 where ent.fechaarchivo BETWEEN @sFECHAFRST AND @sFECHALAST and regf.codproducto <> @pCODPRODUCTO
+                 group by regf.codentradaarchivocontrol, regf.codtiporegistro,regf.codciudad, regf.codpuntodeventa, 
+                 rfr.fechahoratransaccion, rfr.suscriptor
+                ) reg on reg.codpuntodeventa = rf.codpuntodeventa
+            where fechaarchivo BETWEEN @sFECHAFRST AND @sFECHALAST and rf.codproducto = @pCODPRODUCTO
+            group by e.fechaarchivo, rf.codpuntodeventa,p.codigogtechpuntodeventa,
+             r.nomregional, rfr.suscriptor, c.nomciudad, p.nompuntodeventa,  reg.codpuntodeventa, 
+             reg.suscriptor
+             ) T
+         group by codpuntodeventa, fechaarchivo, codigogtechpuntodeventa,
+          nompuntodeventa, suscriptor, horas, totalminutos, nomregional, ciudad, minutos
+          order by 1;
+
+END
 GO
 
 
